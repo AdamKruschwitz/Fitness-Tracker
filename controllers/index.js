949ae60ced53f3324aa7f156e226335481d155ca
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-11-15 15:59:52 -0800

    added static root routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,14 @@
+const { response } = require('express');
+const path = require('path');
+
 const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
 
 module.exports = router;
