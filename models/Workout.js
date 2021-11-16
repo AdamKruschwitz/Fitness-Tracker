fea9a1dba9b0c0e80934d74d4f4de19a962a4710
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-11-15 16:31:39 -0800

    reformatted workout model to use aggregate functions

--- models/Workout.js
+++ models/Workout.js
@@ -7,24 +7,8 @@ const workoutSchema = new Schema({
         type: Date,
         default: Date.now,
     },
-    // Duration in minutes
-    totalDuration: {
-        type: Number,
-    },
-    numExercises: {
-        type: Number
-    },
-    totalWeight: {
-        type: Number
-    },
-    totalSets: {
-        type: Number
-    },
-    totalReps: {
-        type: Number
-    },
-    totalDistance: {
-        type: Number
+    exercises: {
+        type: Object
     }
 });
 
