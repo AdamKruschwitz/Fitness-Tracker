9077ebef4fe29dfecf40985e7e7cf578a68303d1
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-11-15 16:35:14 -0800

    implemented workouts post route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -13,7 +13,13 @@ router.put('/workouts', async (req, res) => {
 
 // Create Workout
 router.post('/workouts', async (req, res) => {
-    // TODO
+    try {
+        const workout = await Workout.create(req.body);
+        res.status(200).json(workout);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({response: "Something went wrong :(", error: err});
+    }
 });
 
 // Get workouts in range
