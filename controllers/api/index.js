179e10b1572f95906df40f2437da27371bbd0e65
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-11-15 16:04:05 -0800

    added skeleton routes

---
+++ controllers/api/index.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+// Get last workout
+router.get('/workouts', async (req, res) => {
+    // TODO
+});
+
+// Add exercise
+router.put('/workouts', async (req, res) => {
+    // TODO
+});
+
+// Create Workout
+router.post('/workouts', async (req, res) => {
+    // TODO
+});
+
+// Get workouts in range
+router.get('/workouts/range', async (req, res) => {
+    // TODO
+});
+
+module.exports = router;
